63e86a25f9739a52ee94307efe4ff3f666e7abdb
petronius <michael.schuller@artlogic.net>
2013-12-19 10:20:58 +0000

    Cleanup old stuff

--- bash/bash_profile.sh
+++ bash/bash_profile.sh
@@ -8,8 +8,8 @@ export LOGIN_SHELL=$false
 source $HOME/.utils/bash/system.sh
 source $HOME/.utils/bash/common.sh
 
-# Don't include work stuff in this (external) repository, but source it from
-# it's place in the SVN-tracked tree.
+# Work machine stuff. Don't include work stuff in this (external) repository,
+# but source it from it's place in the SVN-tracked tree.
 
 if [ -f "$HOME/workspace/Dev/Michael/etc/bash_profile" ]; then
     source $HOME/workspace/Dev/Michael/etc/bash_profile
