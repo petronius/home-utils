424069bab704a864894a1084a0520f4a7c5664d7
petronius <michael.schuller@artlogic.net>
2014-02-06 12:14:36 +0000

    Tweak xmonad.hs in an effort to tame Conky/dzen

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,49 +1,37 @@
--------------------------------------------------------------------------------
--- |
---
--- xmonad.hs, pbrisbin 2012
---
--- <https://github.com/pbrisbin/xmonad-config>
---
--------------------------------------------------------------------------------
+-- I really have a very vague idea of what this stuff does.
+
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.UrgencyHook ( NoUrgencyHook(NoUrgencyHook), withUrgencyHook )
 import XMonad.Util.EZConfig
+import XMonad.Util.Run ( spawnPipe )
 import XMonad.Util.WorkspaceCompare
 
 main :: IO ()
 main = do
-    conf <- statusBar "conky | dzen2 -ta l -w 1600 -p -xs 1 -fn Verdana-10 -e 'onstart=lower'"
-                dzenPP
-                    { ppHidden = pad
-                    , ppTitle  = pad . dzenColor "#bbb" "" . dzenEscape
-                    , ppLayout = const ""
-                    , ppSort   = getSortByXineramaRule
-                    }
-                toggleStrutsKey
-                $ withUrgencyHook NoUrgencyHook
-                $ defaultConfig
-                    { normalBorderColor = "#181818"
-                    , focusedBorderColor = "#555555"
-                    , borderWidth = 2
-                    , terminal   = "urxvt"
-                    , clickJustFocuses = True
-                    , manageHook = composeAll
-                        [ isFullscreen --> doFullFloat
-                        , manageHook defaultConfig
-                        ]
-                    }
-                    `additionalKeysP`
-                        [ ("M-p", spawn "x=$(yeganesh -x -- -i -fn Verdana-12) && exec $x")
-                        , ("M-q", spawn "killall dzen2; xmonad --recompile && xmonad --restart")
-                        , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 3%+")
-                        , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
-                        , ("<XF86AudioMute>",        spawn "amixer sset Master toggle")
-                        ]
+    conky  <- spawnPipe dzenConky
 
-    xmonad conf
+    xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
+    {
+          normalBorderColor = "#181818"
+        , focusedBorderColor = "#555555"
+        , borderWidth = 2
+        , terminal   = "urxvt"
+        , clickJustFocuses = True
+        , manageHook = composeAll
+            [ isFullscreen --> doFullFloat
+            , manageHook defaultConfig
+            ]
+    } `additionalKeysP`
+        [ ("M-q", spawn "killall dzen2; xmonad --recompile && xmonad --restart")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 3%+")
+        , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
+        , ("<XF86AudioMute>",        spawn "amixer sset Master toggle")
+    ]
+
+-- 
+dzenConky = "conky -c ~/.conkyrc | dzen2 -ta l -w 1600 -p -xs 1 -fn Verdana-10 -e 'onstart=lower'"
 
 -- | The unexported X.H.DynamicLog.toggleStrutsKey
 toggleStrutsKey :: XConfig l -> (KeyMask, KeySym)
